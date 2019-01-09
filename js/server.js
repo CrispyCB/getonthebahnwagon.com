3a95e9d4bb0b8acd04d75fb17efb0ea03bb70040
Christian Bahnweg <cebahnweg@gmail.com>
2019-01-08 22:46:52 -0500

    adds path

--- js/server.js
+++ js/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
-app.set('views', './views')
+app.set('views', '../views')
 app.set('view engine', 'pug')
+app.use('/stylesheets', express.static(path.join( __dirname, 'stylesheets')));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/about', (req, res) => res.render('about'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
