c8e1f5a3b8453b7a81212533bbbced7d0e728ebe
Christian Bahnweg <cebahnweg@gmail.com>
2019-01-11 22:57:30 -0500

    adds assets route and finishes documentation.pug

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = 3000
 app.set('views', './views')
 app.set('view engine', 'pug')
 app.use('/stylesheets', express.static(path.join( __dirname, 'stylesheets')));
+app.use('/assets', express.static(path.join(__dirname, '/assets')));
 
 app.get('/about', (req, res) => res.render('about'))
 app.get('/about/communicator', (req, res) => res.render('communicator'))
